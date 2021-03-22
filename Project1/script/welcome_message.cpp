c5d8dbe8d16eab81532edac1d6b7ef46afc26065
GLeurquin <guillaume.leurquin@gmail.com>
2021-03-23 00:18:18 +0100

    added project 1

---
+++ Project1/script/welcome_message.cpp
@@ -0,0 +1,19 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo {
+class WorldPluginWelcome : public WorldPlugin {
+public:
+
+  WorldPluginWelcome() : WorldPlugin()
+  {
+    printf("Welcome to Guillaume’s World!!\n");
+  }
+
+public:
+
+  void
+  Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+  {}
+};
+GZ_REGISTER_WORLD_PLUGIN(WorldPluginWelcome)
+}
